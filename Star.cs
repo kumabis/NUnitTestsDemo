99290983bc5eaddfadc3738ad569fb1a7aac8ace
Bishal Kumar <Bishal.Kumar@parentpay.com>
2023-06-05 12:50:39 +0530

    Add project files.

---
+++ Star.cs
@@ -0,0 +1,26 @@
+﻿namespace WebApplication1
+{
+    public class Star
+    {
+        public double Id { get; set; }
+        public string? Name { get; set; }
+
+        public Star(double id)
+        {
+            Id = id;
+        }
+
+        public double Shine()
+        {
+            if(Id != -1)
+                return Id;
+            else
+                throw new InvalidOperationException();
+        }
+
+        public void FadeOut()
+        {
+            Id = -1;//adds condition for throwing new InvalidOperationException();
+        }
+    }
+}
---
+++ Tests/Test.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using WebApplication1;
+
+[TestFixture]
+public class Test
+{
+    [Test]
+    public void NewStarsCanShine()
+    {
+        double shineFactor = 1d;
+        Star star = new Star(shineFactor);
+        //Assert.AreEqual(shineFactor, star.Shine());
+        Assert.That(star.Shine(), Is.EqualTo(shineFactor));
+    }
+
+    [Test]
+    public void FadedOutStarsCannotShine()
+    {
+        double shineFactor = 1.5d;
+        Star star = new Star(shineFactor);
+        star.FadeOut();
+        Assert.Throws(typeof(InvalidOperationException), () => star.Shine());
+    }
+
+    [Test]
+    public void StarsCanBeNAmes()
+    {
+        double shineFactor = 0.8d;
+        Star star = new Star(shineFactor);
+        star.Name = "Sun";
+
+        //Assert.AreEqual("Sun", star.Name);
+        Assert.That(star.Name, Is.EqualTo("Sun"));
+    }
+}
